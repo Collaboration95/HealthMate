3d470b98db6c0852f333eda50c376070ece8f88a
mrmrjing <hongjing123321@gmail.com>
2023-03-29 16:42:46 +0800

    Base App Test Commit 1

---
+++ app/src/main/java/com/example/healthmate/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.healthmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    button = findViewById(R.id.button1);
+    button.setOnClickListener(view -> {
+        Toast.makeText(MainActivity.this,"THis is a message", Toast.LENGTH_LONG).show();
+    });
+
+
+    }
+
+}
